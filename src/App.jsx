170f2b56022310cfbc692aec40d08c0796ddc50d
V-ESPINOZA <vrespinoza@cased.edu.pe>
2025-09-24 15:49:05 -0500

    header y app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Header from "./components/Header";
 import { data } from "./data/data";
 
 function App() {
@@ -5,37 +6,58 @@ function App() {
 
   return (
     <>
-      <section className="py-5">
-        <div className="container text-center">
-          <h1>Happy Store</h1>
-          <p>Welcome to Happy Store</p>
-        </div>
-      </section>
-      <section className="py-4">
-        <div className="container">
-          <h2>Products</h2>
+      <Header />
+      <main>
+        <section className="py-5">
+          <div className="container text-center">
+            <h1>Happy Store</h1>
+            <p>Welcome to Happy Store</p>
+          </div>
+        </section>
+        <section className="py-4">
+          <div className="container">
+            <h2>Products</h2>
 
-  <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
-            {data.map(item => {
-              
-              const { id, title, price, description, category, image, rating } = item;
-              return (
-             <div className="col" key={id}>
-                  <div className="card">
-                    <img src={image} className="card-img-top object-fit-contain" alt={title} height={200} />
-                    <div className="card-body">
-                      <h4 className="card-title">{title} <span className="h6">{rating.rate} ⭐</span></h4>
-                      <h5 class="card-subtitle mb-2 text-body-secondary">{category}</h5>
-                      <p className="card-text">{description.slice(0, 128)}...</p>
-                      <h5>${price}</h5>
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
+              {data.map((item) => {
+                const {
+                  id,
+                  title,
+                  price,
+                  description,
+                  category,
+                  image,
+                  rating,
+                } = item;
+                return (
+                  <div className="col" key={id}>
+                    <div className="card">
+                      <img
+                        src={image}
+                        className="card-img-top object-fit-contain"
+                        alt={title}
+                        height={200}
+                      />
+                      <div className="card-body">
+                        <h4 className="card-title">
+                          {title} <span className="h6">{rating.rate} ⭐</span>
+                        </h4>
+                        <h5 className="card-subtitle mb-2 text-body-secondary">
+                          {category}
+                        </h5>
+                        <p className="card-text">
+                          {description.slice(0, 128)}...
+                        </p>
+                        <h5>${price}</h5>
+                      </div>
                     </div>
                   </div>
-                </div>)
-                 })}
+                );
+              })}
             </div>
-
-        </div>
-      </section>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,33 @@
+const Header = () => {
+  return (
+    <header className="fixed-top">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container">
+          <a className="navbar-brand" href="#">HS 🛒</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link" href="#">Productos</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Promociones</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contacto</a>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-primary" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
