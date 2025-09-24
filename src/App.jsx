4cdace94d41872a1f70109102f32df9e53a33bfe
V-ESPINOZA <vrespinoza@cased.edu.pe>
2025-09-24 15:09:46 -0500

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import { data } from "./data/daja"
+import { data } from "./data/data";
 
 function App() {
-  console.log(data)
-   return (
-      <h1>Happy Store</h1>
-   )
+  console.log(data);
+
+  return (
+    <>
+      <section className="py-5">
+        <div className="container text-center">
+          <h1>Happy Store</h1>
+          <p>Welcome to Happy Store</p>
+        </div>
+      </section>
+      <section className="py-4">
+        <div className="container">
+          <h2>Products</h2>
+          <ul>
+        {data.map(item => {
+          const {id, title} = item;
+          return (
+            <li key={id}>{title}</li>
+          );
+        })}
+          </ul>
+        </div>
+      </section>
+    </>
+  );
 }
-export default App
+export default App;
